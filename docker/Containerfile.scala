5fbc8f2249727b9359d790852fca37b3e2bd7b87
j000 <jarymut+github@gmail.com>
2021-03-10 09:11:42 +0100

    What your doctor isn't telling you about containers

---
+++ docker/Containerfile.scala
@@ -0,0 +1,23 @@
+# /!\ do NOT mix stable + edge! /!\
+FROM alpine:edge
+
+WORKDIR /root
+
+VOLUME ["/root/project"]
+
+# testing is not enabled by default
+RUN echo 'https://alpine.global.ssl.fastly.net/alpine/edge/testing/' >> /etc/apk/repositories
+
+# install
+RUN apk add --no-cache sbt openjdk8
+
+# sbt should use global cache, so let's download some shit... it will need to happen anyway
+# /!\ this takes ages... /!\
+RUN sbt scalaVersion
+
+ENV JAVA_HOME=/usr/lib/jvm/java-1.8-openjdk PATH="${JAVA_HOME}/bin:${PATH}"
+
+# default for play (scala)
+EXPOSE 9000
+
+# vim: set expandtab ft=dockerfile:
